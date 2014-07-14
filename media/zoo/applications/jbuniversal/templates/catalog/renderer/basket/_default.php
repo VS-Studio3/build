a70b339ddd3c0bb821ceffa43d3694812f2adada
lemeshko2409 <lemeshko2409@gmail.com>
2014-07-14 13:13:27 +0300

    покупки

--- media/zoo/applications/jbuniversal/templates/catalog/renderer/basket/_default.php
+++ media/zoo/applications/jbuniversal/templates/catalog/renderer/basket/_default.php
@@ -9,6 +9,7 @@
  * @license     http://jbzoo.com/license-pro.php JBZoo Licence
  * @coder       Denis Smetannikov <denis@jbzoo.com>
  */
+
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
@@ -18,58 +19,15 @@ $this->app->jbassets->basket();
 $this->app->jbassets->initJBPrice();
 ?>
 <script type="text/javascript">
-    $j(function() {
-        $j.datepicker.regional['ru'] = {
-            closeText: 'Закрыть',
-            prevText: '&#x3c;Пред',
-            nextText: 'След&#x3e;',
-            currentText: 'Сегодня',
-            monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-                'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-            monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
-                'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-            dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-            dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
-            dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-            weekHeader: 'Не',
-            dateFormat: 'dd.mm.yy',
-            firstDay: 1,
-            isRTL: false,
-            showMonthAfterYear: false,
-            yearSuffix: ''};
-        $j.datepicker.setDefaults($j.datepicker.regional['ru']);
-
+    $j(function(){
         $j('#basket').addClass('active');
         $j('#order_form, #pay_for_products').addClass('un_active');
 
-        $j('.order_form, .pay_for_products, .required-info, #yoo-zoo h1, #joomla_template_dostavka').hide();
+        $j('.order_form, .pay_for_products, .required-info, #yoo-zoo h1').hide();
         $j('#item-submission').prev('h2').hide();
 
-        $j("#joomla_template_samovuvoz input:radio").eq(0).prop("checked", true);
-        $j("#joomla_template_dostavka input:radio").eq(0).prop("checked", true);
-
-        $j("#joomla_template_dostavka input:text:last").datepicker({
-            onSelect: function(dateText, inst) {
-                $j('#current_date').html(inst.selectedDay);
-            }
-        });
-
-        var isDostavkaKureromChecked = false;
-
-        //Самовывоз или доставка курьером
-        $j('#joomla_template_samovuvoz input:radio').click(function() {
-            if ($j(this).val() == 'dostavka-kurerom') {
-                $j('#joomla_template_dostavka').slideDown(500);
-                isDostavkaKureromChecked = true;
-            }
-            else {
-                $j('#joomla_template_dostavka').slideUp(500);
-                isDostavkaKureromChecked = false;
-            }
-        });
-
         //Переход на способ доставки
-        $j('#go_to_order').click(function() {
+        $j('#go_to_order').click(function(){
             $j('#order_form').addClass('active').removeClass('un_active');
             $j('#basket').removeClass('active').addClass('un_active');
 
@@ -78,35 +36,24 @@ $this->app->jbassets->initJBPrice();
         });
 
         //Переход на способ оплаты
-        $j('#go_to_paying').click(function() {
-            var isDataValid = true;
+        $j('#go_to_paying').click(function(){
+            var isDataValid = false;
 
-            if ($j('.order_form input:text').eq(0).val().length == 0
-                    || $j('.order_form input:text').eq(2).val().length == 0
-                    || $j('.order_form input:text').eq(3).val().length == 0) {
-                isDataValid = false;
+            if($j('.order_form input[type="text"]').eq(0).val().length == 0
+                || $j('.order_form input[type="text"]').eq(2).val().length == 0
+                || $j('.order_form input[type="text"]').eq(3).val().length == 0){
+
+                alert('Заполните поля, отмеченные *');
             }
 
-            if (isDostavkaKureromChecked) {
-                $j('#joomla_template_dostavka input:text').each(function() {
-                    if ($j(this).val().length == 0) {
-                        isDataValid = false;
-                    }
-                });
-            }
-
-            if (isDataValid) {
+            if(isDataValid){
                 $j('#pay_for_products').addClass('active').removeClass('un_active');
                 $j('#order_form').removeClass('active').addClass('un_active');
 
                 $j('.order_form').hide();
                 $j('.pay_for_products').show();
             }
-            else
-                alert('Заполните поля, отмеченные *');
         });
-
-
     });
 </script>
 <div id="basket">Покупки</div>
@@ -114,7 +61,6 @@ $this->app->jbassets->initJBPrice();
 <div id="pay_for_products">Оплата</div>
 
 <div class="basket">
-<<<<<<< HEAD
     <div class="jbbasket-table jsJBZooBasket">
 	<div>
     <thead>
@@ -146,67 +92,12 @@ $this->app->jbassets->initJBPrice();
         $basketItem['price'] = $this->app->jbmoney->convert($basketItem['currency'], $currencyConvert, $basketItem['price']);
 
         $count += $basketItem['quantity'];
-=======
-    <table class="jbbasket-table jsJBZooBasket">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th><?php echo JText::_('JBZOO_CART_ITEM_SKU'); ?></th>
-                <th></th>
-                <th><?php echo JText::_('JBZOO_CART_ITEM_NAME'); ?></th>
-                <th style="min-width: 70px;"><?php echo JText::_('JBZOO_CART_ITEM_PRICE'); ?></th>
-                <th><?php echo JText::_('JBZOO_CART_ITEM_QUANTITY'); ?></th>
-                <th><?php echo JText::_('JBZOO_CART_ITEM_SUBTOTAL'); ?></th>
-                <th></th>
-            </tr>
-        </thead>
 
-        <tbody>
-            <?php
-            $i = 0;
-            $summa = 0;
-            $count = 0;
+        $subtotal = $basketItem['quantity'] * $basketItem['price'];
+        $summa += $subtotal;
 
-            $currencyConvert = $view->appParams->get('global.jbzoo_cart_config.currency');
-            $imageElementId = $view->appParams->get('global.jbzoo_cart_config.element-image');
+        $image = $this->app->jbitem->renderImageFromItem($item, $imageElementId, true);
 
-            foreach ($view->basketItems as $hash => $basketItem) {
-
-                $item = $basketItem['item'];
-
-                $basketItem['price'] = $this->app->jbmoney->convert($basketItem['currency'], $currencyConvert, $basketItem['price']);
-
-                $count += $basketItem['quantity'];
-
-                $subtotal = $basketItem['quantity'] * $basketItem['price'];
-                $summa += $subtotal;
-
-                $image = $this->app->jbitem->renderImageFromItem($item, $imageElementId, true);
-
-                echo '<tr class="row-' . $hash . '" data-itemId="' . $item->id . '" data-hash="' . $hash . '">';
-                echo '<td>' . ++$i . '</td>';
-                echo '<td>' . $basketItem['sku'] . '</td>';
-                echo '<td>' . $image . '</td>';
-
-                echo '<td>';
-                echo '<a href="' . $this->app->route->item($item) . '" title="' . $item->name . '">' . $item->name . '</a>';
-
-                if (isset($basketItem['priceParams']) && !empty($basketItem['priceParams'])) {
-                    foreach ($basketItem['priceParams'] as $key => $value) {
-                        if (!empty($value)) {
-                            echo '<div><strong>' . $key . ':</strong> ' . $value . '</div>';
-                        }
-                    }
-                }
->>>>>>> 777f224dbdb430f14792a0087a7f436cbccf7009
-
-                if (!empty($basketItem['priceDesc'])) {
-                    echo '<br/><span class="price-description">' . $basketItem['priceDesc'] . '</span>';
-                }
-
-                echo '</td>';
-
-<<<<<<< HEAD
         echo '<div class="row-' . $hash . ' border_for_products" data-itemId="' . $item->id . '" data-hash="' . $hash . '">';
      //   echo '<td>' . ++$i . '</td>';
        // echo '<td>' . $basketItem['sku'] . '</td>';
@@ -214,28 +105,13 @@ $this->app->jbassets->initJBPrice();
 echo '<div class="about_buy_product">';
         echo '<div class="name_product">';
         echo '<a href="' . $this->app->route->item($item) . '" title="' . $item->name . '">' . $item->name . '</a>';
-=======
-                if ($basketItem['price']) {
-                    echo '<td class="jsPricevalue" price="' . $basketItem['price'] . '">'
-                    . $this->app->jbmoney->toFormat($basketItem['price'], $currencyConvert)
-                    . ' </td>';
-                } else {
-                    echo '<td> - </td>';
+
+        if (isset($basketItem['priceParams']) && !empty($basketItem['priceParams'])) {
+            foreach ($basketItem['priceParams'] as $key => $value) {
+                if (!empty($value)) {
+                    echo '<div><strong>' . $key . ':</strong> ' . $value . '</div>';
                 }
-
-                echo '<td><input type="text" class="jsQuantity input-quantity" value="' . $basketItem['quantity'] . '" /></td>';
->>>>>>> 777f224dbdb430f14792a0087a7f436cbccf7009
-
-                if ($basketItem['price']) {
-                    echo '<td class="jsSubtotal">' . $this->app->jbmoney->toFormat($subtotal, $currencyConvert) . '</td>';
-                } else {
-                    echo '<td> - </td>';
-                }
-
-                echo '<td><input type="button" class="jbbutton jsDelete" itemid="' . $item->id . '" value="' . JText::_('JBZOO_CART_DELETE') . '" /></td>';
-                echo "</tr>\n";
             }
-<<<<<<< HEAD
         }
 
         if (!empty($basketItem['priceDesc'])) {
@@ -279,36 +155,16 @@ echo '<div class="about_buy_product">';
    
     </tfoot>
 </div>
-=======
-            ?>
-        </tbody>
-
-        <tfoot>
-            <tr>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td>&nbsp;</td>
-                <td><strong><?php echo JText::_('JBZOO_CART_TOTAL'); ?>:</strong></td>
-                <td class="jsTotalCount"><?php echo $count; ?></td>
-                <td class="jsTotalPrice"><?php echo $this->app->jbmoney->toFormat($summa, $currencyConvert); ?></td>
-                <td>
-                    <input type="button" class="jbbutton jsDeleteAll" value="<?php echo JText::_('JBZOO_CART_REMOVE_ALL'); ?>"/>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
->>>>>>> 777f224dbdb430f14792a0087a7f436cbccf7009
     <div id="go_to_order">Продолжить</div>
 </div>
 
 <script type="text/javascript">
-    jQuery(function($) {
+    jQuery(function ($) {
         $('.jbzoo .jsJBZooBasket').JBZooBasket({
-            'clearConfirm': "<?php echo JText::_('JBZOO_CART_CLEAR_CONFIRM'); ?>",
-            'quantityUrl': "<?php echo $this->app->jbrouter->basketQuantity($view->appId); ?>",
-            'deleteUrl': "<?php echo $this->app->jbrouter->basketDelete($view->appId); ?>",
-            'clearUrl': "<?php echo $this->app->jbrouter->basketClear($view->appId); ?>"
+            'clearConfirm': "<?php echo JText::_('JBZOO_CART_CLEAR_CONFIRM');?>",
+            'quantityUrl' : "<?php echo $this->app->jbrouter->basketQuantity($view->appId);?>",
+            'deleteUrl'   : "<?php echo $this->app->jbrouter->basketDelete($view->appId);?>",
+            'clearUrl'    : "<?php echo $this->app->jbrouter->basketClear($view->appId);?>"
         });
     });
 </script>
